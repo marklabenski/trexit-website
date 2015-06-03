1078b1f92a4d8d946bbc90d407b287b45b756097
marklabenski <mark.labenski@gmail.com>
2015-06-03 17:11:06 +0200

    first files, added boot flat, require js etc.

---
+++ js/init.js
@@ -0,0 +1,11 @@
+/**
+ * Created by marklabenski on 03.06.15.
+ */
+requirejs.config({
+    // ... config ...
+    paths: {
+        "bootflat": 'bower_components/Bootflat/js/site.min.js',
+        "loadash": 'bower_components/lodash/dist/lodash'
+    }
+    // ... config ...
+});
